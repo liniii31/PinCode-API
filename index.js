4d981ad8f49c69970a6320384ce28126ccd57e4e
Shalini Anand <42630854+liniii31@users.noreply.github.com>
2022-12-21 17:23:55 +0530

    changes

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-window.onload = function () {
-    let username_error = document.getElementById('username-error');
-    let email_error = document.getElementById('email-error');
-    let password_error = document.getElementById('password-error');
-    let confirm_error = document.getElementById('confirm-error');
-    let submit = document.getElementById('submit');
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let options = {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': 'e4038346ebmsh18f8d9e4d66ee70p191150jsncacb55c7eb77',
+        'X-RapidAPI-Host': 'get-details-by-pin-code-india.p.rapidapi.com'
+    },
+    body: '{"pincode":"751004"}'
+};
 
-    submit.addEventListener('click', function () {
-        username_error.style.display = "none";
-        email_error.style.display = "none";
-        password_error.style.display = "none";
-        confirm_error.style.display = "none";
-        if (document.getElementById('username').value === "") {
-            username_error.textContent = "User name is required.";
-            username_error.style.display = "block";
-        }
-        if (document.getElementById('email').value === "") {
-            email_error.textContent = "Email is required.";
-            email_error.style.display = "block";
-        }else{
-            if(!document.getElementById('email').value.match(validRegex)){
-                email_error.textContent = "Email format is Wrong.";
-                email_error.style.display = "block";
-            }
-        }
-        if (document.getElementById('password').value == "") {
-            password_error.textContent = "Password is required.";
-            password_error.style.display = "block";
-        }
-        if (document.getElementById('confirm-password').value == "") {
-            confirm_error.textContent = "Confirm Password is required.";
-            confirm_error.style.display = "block";
-        }        
-        if(document.getElementById('password').value.length<8){
-            password_error.textContent = "Enter atleast 8 characters";
-            password_error.style.display = "block";
-        }
-        if(document.getElementById('password').value !== document.getElementById('confirm-password').value){
-            confirm_error.textContent = "Confirm Password is not same.";
-            confirm_error.style.display = "block";
-        }
-        if(username_error.style.display == "none" && email_error.style.display == "none" && password_error.style.display == "none" && password_error.style.display == "none" && confirm_error.style.display == "none"){
-            alert('Submitted Successfully')
-        }
-    })
-}
+let submit = document.getElementById('search');
+submit.addEventListener('click', function () {
+    document.getElementById('show').innerHTML = "";
+    options.body = `{"pincode":"${document.getElementById('pincode').value}"}`;
+    fetch('https://get-details-by-pin-code-india.p.rapidapi.com/detailsbypincode', options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            let d = document.createElement('div');
+            d.innerHTML = `City:${response.details[0]["city_name"]}<br>District:${response.details[0]["district_name"]}<br>State:${response.details[0]["state_name"]}`;
+            document.getElementById('show').appendChild(d);
+
+        })
+        .catch(err => console.error(err));
+})
